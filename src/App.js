2ac42affd0c1ec4470b2d9da038fdcdbe6527e52
Ravi Kiran Kanculakunta <kancravi@us.ibm.com>
2020-12-23 00:38:25 -0800

    Initial check in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import useBooksSearch from './useBooksSearch';
+import {useState, useRef, useCallback } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import {Card,  Row, Col, Container, Form} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+	const [query, setQuery] = useState('');
+	const [pageNumber,setPageNumber] = useState('')
+	const { books,loading,error, hasMore } = useBooksSearch(query,pageNumber);
+	const observer = useRef();
+	const lastElementRef = useCallback(node => {
+		 if(loading) return;
+		 if(observer.current) observer.current.disconnect();
+		 observer.current = new IntersectionObserver(entries => {
+		 	 if(entries[0].isIntersecting && hasMore) {
+		 	 	setPageNumber(prevPageNumber => prevPageNumber + 1);
+		     }
+		 })
+		 if(node) observer.current.observe(node);
+	}, [loading,hasMore])
+
+
+	function onTextInputChange(e) {
+        setQuery(e.target.value);
+        setPageNumber(1)
+	}
+
+	return (
+		<Container className="p-4">
+			<Row>
+				<Col>
+					<Form>
+						<Form.Group>
+							<Form.Control size="lg" type="text" placeholder="Search Books" value={query} onChange={onTextInputChange}/>
+						</Form.Group>
+					</Form>
+					<Row>
+						{books && books.map((book,index) => {
+							if(index === books.length - 1) {
+								return (
+									<Col className="p-2" sm={12} md={4} ref={lastElementRef} key={`book-${Math.random()}`}>
+										<Card style={{ height: '100%' }}>
+											<Card.Img style={{ maxHeight: '325px' }} className="img-fluid" variant="top" src={`http://covers.openlibrary.org/b/olid/${book.edition_key && book.edition_key[0]}-L.jpg`} />
+											<Card.ImgOverlay>
+												<Card.Body>
+													<Card.Title>{book.title}</Card.Title>
+												</Card.Body>
+											</Card.ImgOverlay>
+										</Card>
+									</Col>
+								)
+							} else {
+								return <Col className="p-2" sm={12} md={4} key={`book-${Math.random()}`}>
+									<Card style={{ height: '100%' }}>
+										<Card.Img style={{ maxHeight: '325px' }} className="img-fluid" variant="top" src={`http://covers.openlibrary.org/b/olid/${book.edition_key && book.edition_key[0]}-L.jpg`} />
+										<Card.ImgOverlay>
+											<Card.Body>
+												<Card.Title>{book.title}</Card.Title>
+											</Card.Body>
+										</Card.ImgOverlay>
+									</Card>
+								</Col>
+							}
+						})}
+					</Row>
+					{loading && <Row>
+						{Array(3).fill("").map(index => {
+							return (
+								<Col className="p-2" sm={12} md={4} ref={lastElementRef} key={`skeleton-${Math.random()}`}>
+									<Card>
+										<Skeleton height={150} />
+										<Card.Body>
+											<Skeleton/>
+										</Card.Body>
+									</Card>
+								</Col>
+							)
+						})}
+					</Row>}
+					{error && <p>Error</p>}
+				</Col>
+			</Row>
+		</Container>
+	);
 }
-
-export default App;
---
+++ src/useBooksSearch.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const URL = 'http://openlibrary.org/search.json';
+
+export default function useBooksSearch (query,pageNumber) {
+	const [books,setBooks] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+	const [hasMore,setHasMore] = useState(true);
+	let cancel
+
+	useEffect(() => {
+		setBooks([])
+	},[query])
+
+	useEffect(() => {
+		setLoading(true);
+		setError(false);
+		axios({
+			method: 'GET',
+			url: URL,
+			params: {q: query, page: pageNumber},
+			cancelToken: new axios.CancelToken(c => cancel = c)
+		}).then(response => {
+			console.log(response.data.docs);
+			setLoading(false);
+			setBooks((prevBooks) => {
+				return [...new Set([...prevBooks, ...response.data.docs])]
+			})
+			setHasMore(response.data.docs.length > 0)
+		}).catch(err => {
+			if(axios.isCancel(err)) return
+			setError(true)
+		})
+		return () => cancel();
+	},[query,pageNumber])
+
+	return {
+		books,
+		loading,
+		error,
+		hasMore
+	};
+}
